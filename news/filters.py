27f10c1f7ad668ec25101e9db771993b4d865ea1
ehsan <ehsanmelm82@gmail.com>
2023-07-25 17:25:22 +0330

    feat : add filteroption base on tags for news

---
+++ news/filters.py
@@ -0,0 +1,10 @@
+from django_filters.rest_framework import FilterSet
+from .models import NewsModel
+
+
+class NewsFilter(FilterSet):
+    class Meta:
+        model = NewsModel
+        fields = {
+            'tags': ['exact'],
+        }
--- news/views.py
+++ news/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render , get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.views import APIView
 from rest_framework.pagination import PageNumberPagination
 from .models import TagModel , NewsModel
 from .serializers import NewsSerializer
+from .filters import NewsFilter
+
 # Create your views here.
 
 class NewsViewSet(ModelViewSet):
     queryset = NewsModel.objects.all()
     serializer_class = NewsSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = NewsFilter
 
     

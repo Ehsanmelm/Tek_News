ff9ea1f7b858ca29a77c6be64ddf747f6c3fa2b5
ehsan <ehsanmelm82@gmail.com>
2023-07-25 22:35:24 +0330

    feat : add test for get request to details of news

--- news/test/test_news.py
+++ news/test/test_news.py
@@ -17,3 +17,6 @@ class TestNews(APITestCase):
         response = self.client.get('/api/news/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
+    def test_news_retrieve(self):
+        response = self.client.get('/api/news/1/')
+        self.assertEqual(response.status_code ,status.HTTP_200_OK)

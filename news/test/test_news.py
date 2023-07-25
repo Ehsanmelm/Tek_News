846e9c838f9a312475a292e91c59f01701476d3f
ehsan <ehsanmelm82@gmail.com>
2023-07-25 22:34:30 +0330

    feat : add test for get request to news list

---
+++ news/test/test_news.py
@@ -0,0 +1,19 @@
+import pytest
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient, APITestCase
+from news.models import NewsModel
+
+
+@pytest.mark.django_db
+class TestNews(APITestCase):
+    client = APIClient()
+
+    def setUp(self):
+        self.news1 = NewsModel.objects.create(title='news1', description='Description1', resources='source1')
+
+    def test_list_news(self):
+        # url = reverse('news_list')
+        response = self.client.get('/api/news/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+

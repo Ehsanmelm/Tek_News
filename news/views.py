8ed7abc8fa3e3ef6d9952e20403528331ed43b0d
ehsan <ehsanmelm82@gmail.com>
2023-07-25 16:28:15 +0330

    feat : add viewset for showing news

--- news/views.py
+++ news/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render , get_object_or_404
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
 from rest_framework.views import APIView
+from rest_framework.pagination import PageNumberPagination
 from .models import TagModel , NewsModel
-
+from .serializers import NewsSerializer
 # Create your views here.
 
+class NewsViewSet(ViewSet.ReadOnlyModelViewSet):
+    queryset = NewsModel.objects.all()
+    serializer_class = NewsSerializer
+    
+    

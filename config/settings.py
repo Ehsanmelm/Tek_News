a105ddb97c6017018b1b18cd3d7dba3a31cf6f03
ehsan <ehsanmelm82@gmail.com>
2023-07-29 16:41:33 +0330

    feat : add environment varialbe to the project

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+import os,environ 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,11 +21,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
+env = environ.Env(
+    # Default values
+    DEBUG = (bool , True),
+    SECRET_KEY = (str , 'django-insecure-ji**2pmiv6(9-zzle@5old#!px7dt6+ko%&!9@jh2w72ewz$rp'),
+)
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ji**2pmiv6(9-zzle@5old#!px7dt6+ko%&!9@jh2w72ewz$rp'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -85,8 +94,8 @@ DATABASES = {
         'default': {  
         'ENGINE': 'django.db.backends.mysql',  
         'NAME': 'tek_newws_db',  
-        'USER': 'ehsan',  
-        'PASSWORD': 'ehsan1382',  
+        'USER': env('DB_USER'),  
+        'PASSWORD': env('DB_PASSWORD'),  
         'HOST': '127.0.0.1',  
         'PORT': '3306',    
 }

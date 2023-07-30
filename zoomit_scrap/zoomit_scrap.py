a2609c58f01d0d64ed4dbbd63997a1479bdae06f
ehsan <ehsanmelm82@gmail.com>
2023-07-30 18:39:56 +0330

    feat : add function to get the necessary fields from zoomit site

--- zoomit_scrap/zoomit_scrap.py
+++ zoomit_scrap/zoomit_scrap.py
@@ -5,3 +5,24 @@ class NewsItem(scrapy.Item):
     title = scrapy.Field()
     content = scrapy.Field()
     url = scrapy.Field()
+
+
+class ZoomitSpider(scrapy.Spider):
+    name = 'zoomit_scrapy'
+    start_urls = ['https://www.zoomit.ir/']
+
+    def parse(self, response):
+
+        articles = response.css(
+            '.box__BoxBase-sc-1ww1anb-0.eIbCri.pages__LeftModuleBox-ghzl0u-4.gRVxfC')
+        for article in articles:
+            item = NewsItem()
+            exit
+            item['title'] = article.css(
+                '.typography__StyledDynamicTypographyComponent-t787b7-0.ibfopD.BrowseArticleListItemDesktop___StyledTypography-sc-1szqe4e-0.hYItiO ::text').getall()
+
+            item['content'] = article.css(
+                '.box__BoxBase-sc-1ww1anb-0.eIbCri.pages__LeftModuleBox-ghzl0u-4.gRVxfC p::text').getall()
+
+            item['url'] = article.css(
+                '.link__CustomNextLink-sc-1r7l32j-0.iCQspp::attr(href)').getall()

89ecf11005d564d680b8c470ed6ef68293a5ae59
ehsan <ehsanmelm82@gmail.com>
2023-08-10 20:56:30 +0330

    feat : add url for view related to ElasticSearch

--- news/urls.py
+++ news/urls.py
@@ -1,8 +1,13 @@
 from . import views
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import DefaultRouter, SimpleRouter
 from django.urls import path
 
 router = DefaultRouter()
 router.register('news', views.NewsViewSet, basename='news_list')
 
-urlpatterns = router.urls
+elastic_router = SimpleRouter(trailing_slash=False)
+elastic_router.register(
+    r'elastic_news', views.NewsDocumentView, basename='elastic_news')
+
+
+urlpatterns = router.urls + elastic_router.urls

22b63940f1d946999f45671c8b1fadc4b2aa0363
ehsan <ehsanmelm82@gmail.com>
2023-08-10 20:58:51 +0330

    feat : add search filed for news base on tags

--- news/tasks.py
+++ news/tasks.py
@@ -6,35 +6,7 @@ from .models import NewsModel
 from elasticsearch_dsl.connections import connections
 
 
-# @shared_task
-# def update_elastic_index():
-
-#     # Connect to Elasticsearch
-#     connections.create_connection(hosts=['elasticsearch'])
-
-#     # Define the Elasticsearch index
-#     index = Index('news')
-
-#     # Add mapping for your model
-#     index.document(NewsModel)
-
-#     # Fetch data from the database
-#     data = NewsModel.objects.all()  # Replace with your query
-
-#     # Delete previous documents
-#     index.delete()
-
-#     # Index new documents
-#     for obj in data:
-#         obj.save()
-#         index.save(obj)
-
-#     # Refresh the index
-#     index.refresh()
-
-
 @shared_task
 def NewsScraperTask(message):
     subprocess.call(["python", "manage.py", "search_index", "--rebuild", "-f"])
     cmdline.execute("scrapy runspider zoomit_scrap/recent_news.py".split())
-    # update_elastic_index.delay()
--- news/views.py
+++ news/views.py
@@ -39,22 +39,3 @@ class NewsDocumentView(DocumentViewSet):
     def list(self, request, *args, **kwargs):
         NewsScraperTask.delay('i am sending message')
         return super().list(request, *args, **kwargs)
-
-# class NewsDocumentFilter(django_filters.FilterSet):
-#     tags = django_filters.CharFilter(method='filter_tags')
-
-#     def filter_tags(self, queryset, name, value):
-#         tags = value.split(',')  # Assuming the tags are comma-separated
-#         return queryset.filter(tags__in=tags)
-
-#     class Meta:
-#         model = NewsDocuments
-#         fields = ['tags']
-
-
-# class NewsDocumentView(DocumentViewSet):
-#     document = NewsDocuments
-#     serializer_class = NewsDocumentSerializer
-
-#     filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
-#     filterset_class = NewsDocumentFilter

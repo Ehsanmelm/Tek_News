be3226c2a490784c4845c1cbe42bde8464aee9bd
ehsan <ehsanmelm82@gmail.com>
2023-08-10 20:54:14 +0330

    feat : create documnt base on models fields

---
+++ news/documents.py
@@ -0,0 +1,25 @@
+from django_elasticsearch_dsl import Document, fields
+from django_elasticsearch_dsl.registries import registry
+
+from .models import NewsModel
+
+
+@registry.register_document
+class NewsDocuments(Document):
+    id = fields.TextField()
+    title = fields.TextField()
+    description = fields.TextField(attr='description')
+    tags = fields.TextField(
+        attr='tags',
+        fields={
+            'raw': fields.KeywordField(),
+            # 'suggest': fields.CompletionField(),
+        }
+    )
+    resources = fields.TextField(attr='resources')
+
+    class Index:
+        name = 'news'
+
+    class Django:
+        model = NewsModel

e893db101e2b55265fa68355849988e5361c031a
ehsan moslemi <ehsanmelm82@gmail.com>
2023-08-22 09:00:11 +0000

    feat : add comments for explanation about some way i had change in my code

--- news/models.py
+++ news/models.py
@@ -4,14 +4,18 @@ from django.db import models
 
 
 class NewsModel(models.Model):
+    # id will save in uuid form in scrap part
     id = models.CharField(primary_key=True, max_length=36)
     title = models.TextField(blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     tags = models.TextField(blank=True, null=True)
     resources = models.TextField(blank=True, null=True)
+    # all field have blank and null equal true because of that I had thought it may be a news that doesnt have a part in zoomit site
 
     def __str__(self) -> str:
         return f"{self.title}"
     # class Meta:
     #     # managed = False
     #     db_table = 'newsmodel'
+
+#  i remove tag part because there wasnt any important action on them 
--- news/tasks.py
+++ news/tasks.py
@@ -8,5 +8,6 @@ from elasticsearch_dsl.connections import connections
 
 @shared_task
 def NewsScraperTask(message):
+    # I had handled it by two way(signals and this way ) but because of doing celery task repeatedly i prefered it should be more accurate 
     subprocess.call(["python", "manage.py", "search_index", "--rebuild", "-f"])
     cmdline.execute("scrapy runspider zoomit_scrap/recent_news.py".split())
